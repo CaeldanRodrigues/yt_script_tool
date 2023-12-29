dde39c976e4d9ce3091584e2ade784d761fd5d26
Caeldan Rodrigues <caeldanrodrigues@gmail.com>
2023-12-29 17:47:41 +0530

    Implemented basic submit button functionality

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import streamlit as st
 
 st.title("YouTube Script Generator")
@@ -9,3 +12,17 @@ video_length = st.text_input('Expected Video Length 🕒 (in minutes)',key="vide
 creativity = st.slider('Creativity - (0 LOW - 1 HIGH)', 0.0, 1.0, 0.2,step=0.1)
 
 submit = st.button("Generate Script")
+
+if submit:
+        
+    # call the generate_script function (to be created)
+    st.success('Here is your script ❤️')
+
+    #Display Title
+    st.subheader("Title:🔥")
+
+    #Display Video Script
+    st.subheader("Your Video Script:📝")
+
+    #Display Search Engine Result
+    st.subheader("Check Out - DuckDuckGo Search Results:🔍")
